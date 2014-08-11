e980706f11acf2e7a5da948c23779783644ee377
Marc <elmarco18@yahoo.ie>
2014-08-11 17:15:46 +0100

    dded email to Person class and attributes to the Car with methods

--- src/mainPackage/Car.java
+++ src/mainPackage/Car.java
@@ -2,6 +2,30 @@ package mainPackage;
 
 public class Car {
 
-  private String colour;
+	private String colour;
+	private int numDoors;
+
+	public Car(String colour, int numDoors) {
+		this.colour = colour;
+		this.numDoors = numDoors;
+	}
+
+	public String getColour() {
+		return colour;
+	}
+
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+
+	public int getNumDoors() {
+		return numDoors;
+	}
+
+	public void setNumDoors(int numDoors) {
+		this.numDoors = numDoors;
+	}
+	
+	
 
 }
